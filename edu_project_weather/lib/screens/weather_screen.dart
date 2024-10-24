b6dfcee1bb0f9e3a2ead310879da24236bfc8144
unknown <vlllluv@gmail.com>
2024-10-24 18:19:05 +0900

    weather 1024

--- edu_project_weather/lib/screens/weather_screen.dart
+++ edu_project_weather/lib/screens/weather_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:timer_builder/timer_builder.dart';
-import 'package:timer_builder/timer_builder.dart';
+import 'package:edu_project_weather/model/model.dart';
 import 'package:intl/intl.dart'; //DateFormat을 쓰기위한 패키지
 
 class WeatherScreen extends StatefulWidget {
-  WeatherScreen({this.parseWeatherData});
+  const WeatherScreen({super.key, this.parseWeatherData});
   final dynamic parseWeatherData;
 
   @override
@@ -13,22 +13,29 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
-  late String cityName;
-  late int temp;
+  late String cityName; //두 변수를 이용해 현재 나의 위치와 온도를 가져온다.
+  late int temp; //두 변수를 이용해 현재 나의 위치와 온도를 가져온다.
   var date = DateTime.now();
+
   //스크린상 표현될 값들 선언
+
+  Widget? icon;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    updateData(widget.parseWeatherData);
+    updateData(widget.parseWeatherData); //로딩 라우트에서 받아온 json데이터를 전달.
   }
 
   void updateData(dynamic weatherData) {
+    Model model = Model();
     double temp2 = weatherData['main']['temp'];
-    temp = temp2.round();
-    cityName = weatherData['name'];
+    int condition = weatherData['weather'][0]['id'];
+
+    temp = temp2.round(); //두 변수를 이용해 현재 나의 위치와 온도를 가져온다.
+    cityName = weatherData['name']; //두 변수를 이용해 현재 나의 위치와 온도를 가져온다.
+    icon = model.getWeatherIcon(condition);
 
     print(temp);
     print(cityName);
@@ -48,13 +55,13 @@ class _WeatherScreenState extends State<WeatherScreen> {
         //title: Text(''),
         backgroundColor: Colors.transparent,
         leading: IconButton(
-          icon: Icon(Icons.near_me),
+          icon: const Icon(Icons.near_me),
           onPressed: () {},
           iconSize: 30.0,
         ),
         actions: [
           IconButton(
-            icon: Icon(Icons.location_searching),
+            icon: const Icon(Icons.location_searching),
             onPressed: () {},
             iconSize: 30.0,
           ),
@@ -70,49 +77,188 @@ class _WeatherScreenState extends State<WeatherScreen> {
               height: double.infinity,
             ),
             Container(
-              padding: EdgeInsets.all(20),
+              padding: const EdgeInsets.all(20),
               child: Column(
+                //최상위 컬럼
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              height: 150.0,
+                            ),
+                            Text(cityName,
+                                style: GoogleFonts.lato(
+                                  fontSize: 35.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                )),
+                            Row(
+                              children: [
+                                TimerBuilder.periodic(
+                                    (const Duration(minutes: 1)),
+                                    builder: (context) {
+                                  print(getSystemTime());
+                                  return Text(
+                                    getSystemTime(),
+                                    style: GoogleFonts.lato(
+                                        fontSize: 16.0, color: Colors.white),
+                                  );
+                                }),
+                                Text(
+                                  DateFormat(' - EEEE').format(date),
+                                  style: GoogleFonts.lato(
+                                      fontSize: 16.0, color: Colors.white),
+                                ),
+                                Text(
+                                  DateFormat(' - d MMM, yyy').format(date),
+                                  style: GoogleFonts.lato(
+                                      fontSize: 16.0, color: Colors.white),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          //기온을 넣을 자리
+                          children: [
+                            Text('$temp\u2103',
+                                style: GoogleFonts.lato(
+                                  fontSize: 85.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                )),
+                            Row(
+                              children: [
+                                icon!,
+                                const SizedBox(
+                                  width: 10.0,
+                                ),
+                                Text(
+                                  'Clear Sky',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 16.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
                   Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      SizedBox(
-                        height: 150.0,
+                      const Divider(
+                        height: 15.0,
+                        thickness: 2.0,
+                        color: Colors.white30,
                       ),
-                      Text('Seoul',
-                          style: GoogleFonts.lato(
-                            fontSize: 35.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          )),
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          TimerBuilder.periodic((Duration(minutes: 1)),
-                              builder: (context) {
-                            print('${getSystemTime()}');
-                            return Text(
-                              '${getSystemTime()}',
-                              style: GoogleFonts.lato(
-                                  fontSize: 16.0, color: Colors.white),
-                            );
-                          }),
-                          Text(
-                            DateFormat(' - EEEE').format(date),
-                            style: GoogleFonts.lato(
-                                fontSize: 16.0, color: Colors.white),
+                          Column(
+                            children: [
+                              Text(
+                                'AQI (대기질지수)',
+                                style: GoogleFonts.lato(
+                                  fontSize: 14.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Image.asset(
+                                'image/bad.png',
+                                width: 37.0,
+                                height: 35.0,
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '"매우나쁨"',
+                                style: GoogleFonts.lato(
+                                    fontSize: 14.0,
+                                    color: Colors.black87,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
                           ),
-                          Text(
-                            DateFormat(' - d MMM, yyy').format(date),
-                            style: GoogleFonts.lato(
-                                fontSize: 16.0, color: Colors.white),
+                          Column(
+                            children: [
+                              Text(
+                                '미세먼지',
+                                style: GoogleFonts.lato(
+                                  fontSize: 14.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '174.75',
+                                style: GoogleFonts.lato(
+                                  fontSize: 14.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                'ug/m3',
+                                style: GoogleFonts.lato(
+                                    fontSize: 14.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                '초미세먼지',
+                                style: GoogleFonts.lato(
+                                  fontSize: 14.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '84.03',
+                                style: GoogleFonts.lato(
+                                  fontSize: 14.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                'ug/m3',
+                                style: GoogleFonts.lato(
+                                    fontSize: 14.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
                           )
                         ],
                       )
                     ],
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    //기온을 넣을 자리
                   )
                 ],
               ),
